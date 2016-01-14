48710e031e1fbf7a4e6257de85f0c463d5191807
huaiyudavid <davidyangrocs@gmail.com>
2016-01-14 14:39:16 -0600

    testing
    testing

---
+++ code.py
@@ -0,0 +1,11 @@
+import web
+
+  urls = (
+      '/.*', 'hello',
+      )
+
+  class hello:
+      def GET(self):
+          return "Hello, world."
+
+  application = web.application(urls, globals()).wsgifunc()
