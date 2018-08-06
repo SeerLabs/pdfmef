466cbae72157d728b26e187c39a902f6d5e206b4
Seth Wilcox <snw5208@csxstaging01.ist.psu.edu>
2018-08-06 11:10:33 -0400

    Added support for Grobid 5.0 and replaced pdffigures with pdffigures2.

--- src/extractor/csxextract/config.py
+++ src/extractor/csxextract/config.py
@@ -1,7 +1,7 @@
 import os
 
 # URL to Grobid service
-GROBID_HOST = 'http://localhost:8080'
+GROBID_HOST = 'http://localhost:8070'
 
 # Path to PDFBox jar
 PDF_BOX_JAR = os.path.expanduser('/home/huy138/bin/pdfbox-app-1.8.4.jar')
@@ -14,8 +14,8 @@ FILTER_JAR_PATH = os.path.expanduser('/home/huy138/bin/classifier/classifier.jar
 FILTER_ACL_PATH = os.path.expanduser('/home/huy138/bin/classifier/acl')
 FILTER_TRAIN_DATA_PATH = os.path.expanduser('/home/huy138/bin/classifier/train_str_f43_paper.arff')
 
-# Path to PDFFigures binary
-PDFFIGURES_PATH = os.path.expanduser('/home/huy138/bin/pdffigures')
+# Path to PDFFigures2 JAR
+PDFFIGURES_PATH = os.path.expanduser('/home/huy138/bin/pdffigures2-assembly-0.0.12-SNAPSHOT.jar')
 
 # Path to Algorithms extractor JAR
 ALGORITHMS_JAR_PATH = os.path.expanduser('/home/huy138/bin/algextract/bin/algo_extractor.jar')
