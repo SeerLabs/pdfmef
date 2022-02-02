73c011c0d5b1546a9e7ac938995384f60d462052
Manoj Kumar Angadi <manojkumarangadi@Manojs-MacBook-Air.local>
2022-02-02 12:22:09 -0500

    testing changes

--- src/ingestion/csx_extractor.py
+++ src/ingestion/csx_extractor.py
@@ -29,25 +29,28 @@ class CSXExtractorImpl(CSXExtractor):
 
     def extract_textual_data(self, filepath, source_url):
         print("inside extract_textual_data filepath: \n"+filepath)
-        tei_root = parse(filepath)
-        papers = []
-        paper = Cluster()
-        paper.source_url = source_url
-        tei_filename = str(filepath[str(filepath).rfind('/')+1:])
-        paper_id = tei_filename[:tei_filename.rfind('.')]
-        paper.add_paper_id(paper_id)
-        paper.title = self.extract_title_from_tei_root(tei_root)
-        paper.abstract = self.extract_abstract(tei_root)
-        paper.pub_info = self.extract_paper_pub_info_from_tei_root(tei_root)
-        paper.authors = self.extract_authors_from_tei_root(tei_root)
-        paper.has_pdf = True
-        paper.is_citation = False
-        citations = self.extract_citations_from_tei_root(tei_root=tei_root, paper_id=paper_id)
-        paper.text = self.extract_text_from_tei_root(tei_root)
-        paper.keys = KeyGenerator().get_keys(paper.title, paper.authors)
-        papers.append(paper)
-        papers.extend(citations)
-        print("inside extract_textual_data filepath papers are: \n"+papers)
+        try:
+            tei_root = parse(filepath)
+            papers = []
+            paper = Cluster()
+            paper.source_url = source_url
+            tei_filename = str(filepath[str(filepath).rfind('/')+1:])
+            paper_id = tei_filename[:tei_filename.rfind('.')]
+            paper.add_paper_id(paper_id)
+            paper.title = self.extract_title_from_tei_root(tei_root)
+            paper.abstract = self.extract_abstract(tei_root)
+            paper.pub_info = self.extract_paper_pub_info_from_tei_root(tei_root)
+            paper.authors = self.extract_authors_from_tei_root(tei_root)
+            paper.has_pdf = True
+            paper.is_citation = False
+            citations = self.extract_citations_from_tei_root(tei_root=tei_root, paper_id=paper_id)
+            paper.text = self.extract_text_from_tei_root(tei_root)
+            paper.keys = KeyGenerator().get_keys(paper.title, paper.authors)
+            papers.append(paper)
+            papers.extend(citations)
+            print("inside extract_textual_data filepath papers are: \n"+papers)
+        except Exception as e:
+            print("inside extract_textual_data filepath exception is: "+e)
         return papers
 
     @classmethod
