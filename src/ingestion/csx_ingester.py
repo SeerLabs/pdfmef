3fa53869a2ee47b14f08cb5d3365e72711f619b2
Manoj Kumar Angadi <manojkumarangadi@Manojs-MacBook-Air.local>
2022-04-08 08:01:47 -0400

    s2org matching changes

--- src/ingestion/csx_ingester.py
+++ src/ingestion/csx_ingester.py
@@ -41,6 +41,7 @@ def findMatchingDocumentsS2orcLSH(papers):
     print("inside findMatchingDocumentsS2orcLSH \n")
     config = configparser.ConfigParser()
     config.read("/pdfmef-code/src/extractor/python_wrapper/properties.config")
+    print(config.items)
     elasticConnectionProps = dict(config.items('ElasticConnectionProperties'))
     wrapper = wrappers.ElasticSearchWrapper(elasticConnectionProps)
     print("\n paper --> \n")
