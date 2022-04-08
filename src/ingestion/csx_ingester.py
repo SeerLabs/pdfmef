9377b45461ef22bde0aa949f6f6268c6f69ec37a
Manoj Kumar Angadi <manojkumarangadi@Manojs-MacBook-Air.local>
2022-04-08 08:47:48 -0400

    s2org matching changes

--- src/ingestion/csx_ingester.py
+++ src/ingestion/csx_ingester.py
@@ -114,6 +114,8 @@ class ElasticSearchWrapper(Wrapper):
         print(str(body))
 
         results = self.get_connection().search(index=settings.S2_META_INDEX, body=body)
+        print("\n results\n")
+        print(results)
         self.s2_batch = results['hits']['hits']
 
     def get_document_batch(self):
