449a90dbfd6ab7f9f21ac07c7221b87319c19519
Manoj Kumar Angadi <manojkumarangadi@Manojs-MacBook-Air.local>
2022-04-14 19:23:59 -0400

    s2org matching changes

--- src/ingestion/csx_ingester.py
+++ src/ingestion/csx_ingester.py
@@ -245,19 +245,13 @@ def findMatchingDocumentsS2orcLSH(papers):
             #print("inside findMatchingDocumentsS2orcLSH incoming paper is ---> \n")
             print("\n")
             if (paper.authors and len(paper.authors) > 0 and paper.pub_info):
-                print("year of the paper is-->\n")
-                print(paper.pub_info['year'])
-                print("author of the paper is-->\n")
-                print(paper.authors[0]['fullname'])
-                print("title of the paper is--->\n")
-                print(paper.title)
                 #paper.title = "The beta-decay vicinity of $^{78}$Ni"
                 #paper.authors[0]['fullname'] = "Tetsuya  HAYASHI"
                 #paper.pub_info['year'] = 2011
                  #documents = wrapper.get_s2_batch_for_lsh_matching(paper.authors[0]['fullname'], paper.pub_info['year'])
                 documents = wrapper.get_s2_batch_for_lsh_matching(paper.authors[0]['fullname'], paper.pub_info['year'])
                 #print("inside findMatchingDocumentsS2orcLSH s2orc documents number of documents from s2org query is ---> \n")
-                print(len(documents))
+                #print(len(documents))
                 lsh = MinHashLSH(threshold=0.5, num_perm=128)
                 for doc in documents:
                     title = doc['_source']['title']
@@ -284,9 +278,11 @@ def findMatchingDocumentsS2orcLSH(papers):
                 for shingle in s:
                     min_hash.update(shingle.encode('utf8'))
                 result = lsh.query(min_hash)
-                print("matching documents from the minhash\n")
-                print(result)
+                #print("matching documents from the minhash\n")
+                #print(result)
                 if len(result) > 0:
+                    print("found matching document from the minhash\n")
+                    print(result)
                     mergeMatchingDocs(wrapper, paper, result[0])
 
         except Exception as es:
