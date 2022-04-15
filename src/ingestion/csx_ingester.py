c465912f22c2a74f3757574bb0dfc63db2e8f4cf
Manoj Kumar Angadi <manojkumarangadi@Manojs-MacBook-Air.local>
2022-04-14 20:29:02 -0400

    s2org matching changes

--- src/ingestion/csx_ingester.py
+++ src/ingestion/csx_ingester.py
@@ -251,7 +251,7 @@ def findMatchingDocumentsS2orcLSH(papers):
                 #paper.authors[0]['fullname'] = "Tetsuya  HAYASHI"
                 #paper.pub_info['year'] = 2011
                  #documents = wrapper.get_s2_batch_for_lsh_matching(paper.authors[0]['fullname'], paper.pub_info['year'])
-                documents = wrapper.get_s2_batch_for_lsh_matching(paper.authors[0]['fullname'], paper.pub_info['year'])
+                documents = wrapper.get_s2_batch_for_lsh_matching(paper.authors[0]['surname'], paper.pub_info['year'])
                 #print("inside findMatchingDocumentsS2orcLSH s2orc documents number of documents from s2org query is ---> \n")
                 print(len(documents))
                 lsh = MinHashLSH(threshold=0.5, num_perm=128)
