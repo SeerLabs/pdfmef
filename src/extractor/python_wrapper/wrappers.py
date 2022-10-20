5471a85732519ab598f366accfeac4ffadf8ce46
Manoj Kumar Angadi <manojkumar11930@gmail.com>
2022-10-19 23:12:17 -0400

    pdfmef citation update changes

--- src/extractor/python_wrapper/wrappers.py
+++ src/extractor/python_wrapper/wrappers.py
@@ -316,11 +316,10 @@ class ElasticSearchWrapper(Wrapper):
                   }
                }
 
-        results = self.get_connection_prod().search(index=settings.CLUSTERS_INDEX, body=body)
+        results = self.get_connection_prod().search(index=settings.CLUSTERS_INDEX, body={})
         print("here----------->")
         print(body)
         #print(results)
-        print(str(results))
         self.s2_batch = results['hits']['hits']
 
     def get_document_batch(self):
