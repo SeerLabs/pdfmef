98714ddaf921db3068a148d6aecb6a0080d9b5e0
Manoj Kumar Angadi <manojkumar11930@gmail.com>
2022-10-19 23:03:31 -0400

    pdfmef citation update changes

--- src/ingestion/csx_extractor.py
+++ src/ingestion/csx_extractor.py
@@ -278,6 +278,8 @@ class CSXExtractorImpl(CSXExtractor):
             try:
                 if (paper.pub_info.year):
                     documents = wrapper.get_batch_for_lsh_matching(paper.pub_info.year)
+                    print(documents)
+                    pass
                     lsh = MinHashLSH(threshold=0.7, num_perm=128)
                     for doc in documents:
                         title = doc['_source']['title']
