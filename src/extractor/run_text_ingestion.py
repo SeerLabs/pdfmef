cb441545767bf3e40ff63bce955b93c59aef2b9e
Manoj Kumar Angadi <manojkumar11930@gmail.com>
2022-10-31 15:20:59 -0400

    pdfmef citation update changes

--- src/extractor/run_text_ingestion.py
+++ src/extractor/run_text_ingestion.py
@@ -221,6 +221,7 @@ if __name__ == '__main__':
     import cProfile, pstats
     profiler = cProfile.Profile()
     profiler.enable()
+    print("heree-----")
     main()
     profiler.disable()
     stats = pstats.Stats(profiler).sort_stats('ncalls')
