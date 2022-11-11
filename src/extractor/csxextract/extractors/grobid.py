e99c813ab346fa05049adac61cfe4f23f7a72df3
Manoj Kumar Angadi <manojkumar11930@gmail.com>
2022-11-10 23:51:56 -0500

    pdfmef citation update changes

--- src/extractor/csxextract/extractors/grobid.py
+++ src/extractor/csxextract/extractors/grobid.py
@@ -57,9 +57,10 @@ def _call_grobid_method(data, method):
       #print(url)
       try:
          print(files)
-         print('inside _call_grobid_method grobid time taken----------------------------------->\n')
          resp = requests.post(url, files=files, data=the_data)
-         print(resp.elapsed.total_seconds())
+         if (method == 'processFulltextDocument'):
+            print('inside _call_grobid_method grobid time taken----------------------------------->\n')
+            print(resp.elapsed.total_seconds())
       except requests.exceptions.RequestException as ex:
          print(ex)
          # logging.error("exception while calling Grobid", ex)
