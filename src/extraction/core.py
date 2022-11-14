f241e599f027a5ad35fda5c372df39041ce2788d
Manoj Kumar Angadi <manojkumar11930@gmail.com>
2022-11-14 17:36:24 -0500

    pdfmef citation update changes

--- src/extraction/core.py
+++ src/extraction/core.py
@@ -306,7 +306,7 @@ def _real_run(self, runnables, runnable_props, data, output_dir, **kwargs):
       instance.run_name = run_name
       instance.logger = logging.getLogger('runnables.{0}'.format(runnable.__name__))
       print(instance)
-      result = instance.run(data, dep_results)
+      result = instance.extract(data, dep_results)
       results[runnable] = result
    output_dir = os.path.abspath(os.path.expanduser(output_dir))
    if not os.path.exists(output_dir):
