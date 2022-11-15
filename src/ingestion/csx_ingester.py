129f71d6c3c29a1b97d5bba27288b0e5e8d707ac
Manoj Kumar Angadi <manojkumar11930@gmail.com>
2022-11-15 18:15:06 -0500

    pdfmef citation update changes

--- src/ingestion/csx_ingester.py
+++ src/ingestion/csx_ingester.py
@@ -294,7 +294,7 @@ def mergeMatchingDocs(wrapper, paper, matching_s2org_doc_id):
 
 def ingest_paper_parallel_func(combo):
     papers = CSXExtractorImpl().extract_textual_data(combo[0], combo[2])
-    print("done extraction!")
+    #print("done extraction!")
     #findMatchingDocumentsS2orcLSH(papers)
     move_to_repository(combo[0], combo[1])
     KeyMatcherClusterer().cluster_papers(papers)
