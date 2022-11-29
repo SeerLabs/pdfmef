81a6fd848f1af39dfa1ea9529238d6d059674f18
Manoj Kumar Angadi <manojkumar11930@gmail.com>
2022-11-29 11:02:16 -0500

    pdfmef citation update changes

--- src/extractor/test_citation_matching.py
+++ src/extractor/test_citation_matching.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
     for i in range(4, 5):
         res = es.search(index="dedupe_test", body = {
         "from": i*10000,
-        'size' : 10000,
+        'size' : 10,
         'query': {
             'match_all' : {}
         }
