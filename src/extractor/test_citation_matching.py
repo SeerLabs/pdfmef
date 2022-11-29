2061ec178d74a1622a4b6a204ac8ef93ae31c50f
Manoj Kumar Angadi <manojkumar11930@gmail.com>
2022-11-29 01:54:47 -0500

    pdfmef citation update changes

--- src/extractor/test_citation_matching.py
+++ src/extractor/test_citation_matching.py
@@ -19,11 +19,11 @@ def findMatchingDocumentsLSH(papers):
     for paper in papers:
         try:
             if (True):
-                documents = wrapper.get_batch_for_lsh_matching(paper['_source']['original_title'])
+                documents = wrapper.get_batch_for_lsh_matching(paper['_source']['original_abstract'])
                 lsh = MinHashLSH(threshold=0.90, num_perm=128)
                 for doc in documents:
                     try:
-                        title = doc['_source']['original_title'].lower()
+                        title = doc['_source']['original_abstract'].lower()
                         title = re.sub(r'\s+', ' ', title)
                         id = doc['_source']['core_id']
                         d={}
@@ -38,7 +38,7 @@ def findMatchingDocumentsLSH(papers):
                     except Exception:
                         pass
 
-                Title = paper['_source']['original_title'].lower()
+                Title = paper['_source']['original_abstract'].lower()
                 Title = re.sub(r'\s+', ' ', Title)
                 s = CSXExtractorImpl().create_shingles(Title, 5)
                 min_hash = MinHash(num_perm=128)
@@ -86,13 +86,14 @@ if __name__ == "__main__":
     es = Elasticsearch([{'host': '130.203.139.160', 'port': 9200}])
     mismatch_count = 0
     for i in range(4, 5):
-        res = es.search(index="dedupe_test", body = {
-        "from": i*10000,
-        'size' : 10000,
-        'query': {
-            'match_all' : {}
-        }
-        })
+        res = es.search(index="dedupe_test", body = 
+          "from": 0,
+          "size": 1,
+          "query": {
+            "match": {
+              "core_id.keyword": "25331293"
+            }
+          })
         #print(res)
         print("%d documents found" % res['hits']['total']['value'])
         data = [doc for doc in res['hits']['hits']]
