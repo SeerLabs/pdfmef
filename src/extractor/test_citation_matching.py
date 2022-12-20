47a5520d2ac9672a58f89d0a30ff4061e5a4e376
Manoj Kumar Angadi <manojkumar11930@gmail.com>
2022-12-20 07:44:28 +0530

    pdfmef citation update changes

--- src/extractor/test_citation_matching.py
+++ src/extractor/test_citation_matching.py
@@ -51,6 +51,7 @@ def findMatchingDocumentsLSH(papers, miss_cat_count, match_index = 0):
 
                 else:
                     lsh = MinHashLSH(threshold=0.6, num_perm=128)
+                    print(len(doc))
                     for doc in documents:
                         try:
                             title = doc['_source']['processed_title']
