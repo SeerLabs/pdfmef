d1d6746c043d96bb0d729d45478eb82303e5e940
Manoj Kumar Angadi <manojkumar11930@gmail.com>
2022-12-20 08:13:38 +0530

    pdfmef citation update changes

--- src/extractor/test_citation_matching.py
+++ src/extractor/test_citation_matching.py
@@ -141,6 +141,6 @@ if __name__ == "__main__":
         print(len(docs))
         print("%d documents found" % res['hits']['total']['value'])
         data = [doc for doc in docs]
-        findMatchingDocumentsLSH(data, miss_cat_count, 0)
+        findMatchingDocumentsLSH(data, miss_cat_count, 2)
 
     print('miss classified documents --->', miss_cat_count)
