35fd153b13e1e829ca9018c85ff962132221d17c
Manoj Kumar Angadi <manojkumar11930@gmail.com>
2023-01-24 12:59:24 -0500

    clustering with bm25_lsh for citation changes pdfmef

--- src/ingestion/csx_clusterer.py
+++ src/ingestion/csx_clusterer.py
@@ -60,7 +60,7 @@ class KeyMatcherClusterer(CSXClusterer):
         self.create_new_paper(paper)
 
     def cluster_paper_with_bm25_lsh(self, paper: Cluster) -> None:
-        current_paper_title = "The Magnetic field of the Electric current and the Magnetic induction"
+        current_paper_title = paper.title
         config = configparser.ConfigParser()
         try:
             config.read("/pdfmef-code/src/extractor/python_wrapper/properties.config")
