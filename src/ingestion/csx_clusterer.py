95c072c31ca4b8fae56d1957b6167d3466dc3561
Manoj Kumar Angadi <manojkumar11930@gmail.com>
2023-01-25 18:02:07 -0500

    pdfmef dedupe

--- src/ingestion/csx_clusterer.py
+++ src/ingestion/csx_clusterer.py
@@ -158,6 +158,7 @@ class KeyMatcherClusterer(CSXClusterer):
            print('found similar document with id-->',matched_cluster_id)
            try:
             print('current document title--->', current_paper.title)
+            print('current document title--->', current_paper.paper_id)
            except Exception:
             pass
            resp = Cluster.search(using=self.elastic_service.get_connection()).filter("term", _id=matched_cluster_id[0])
