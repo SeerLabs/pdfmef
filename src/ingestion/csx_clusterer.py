a234906001eecb24b5453194df32874efb2ef8d7
Manoj Kumar Angadi <manojkumar11930@gmail.com>
2023-01-25 00:19:38 -0500

    clustering with bm25_lsh for citation changes pdfmef

--- src/ingestion/csx_clusterer.py
+++ src/ingestion/csx_clusterer.py
@@ -68,14 +68,14 @@ class KeyMatcherClusterer(CSXClusterer):
             except Exception as ex:
                 print(ex)
             elasticConnectionProps = dict(config.items('ElasticConnectionProperties'))
-            wrapper = wrappers.ElasticSearchWrapper(elasticConnectionProps)
-            documents = wrapper.get_batch_for_lsh_matching(current_paper_title)
-            similar_doc_id = self.find_similar_document(documents, current_paper_title)
-            if similar_doc_id and len(similar_doc_id) > 0:
+            #wrapper = wrappers.ElasticSearchWrapper(elasticConnectionProps)
+            #documents = wrapper.get_batch_for_lsh_matching(current_paper_title)
+            #similar_doc_id = self.find_similar_document(documents, current_paper_title)
+            #if similar_doc_id and len(similar_doc_id) > 0:
                 #similar_paper_id = similar_doc_id[0]
-                self.merge_with_existing_cluster(matched_cluster_id=similar_doc_id, current_paper=paper)
-            else:
-                self.create_new_paper(paper)
+            #    self.merge_with_existing_cluster(matched_cluster_id=similar_doc_id, current_paper=paper)
+            #else:
+            self.create_new_paper(paper)
         except Exception as ex:
             print("exception in cluster_paper_with_bm25_lsh with msg-->", ex)
 
