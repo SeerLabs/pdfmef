98212ba87b7f769458b01f755b3c85aa3f8f597e
Manoj Kumar Angadi <manojkumar11930@gmail.com>
2023-01-24 20:39:03 -0500

    clustering with bm25_lsh for citation changes pdfmef

--- src/ingestion/csx_clusterer.py
+++ src/ingestion/csx_clusterer.py
@@ -153,11 +153,13 @@ class KeyMatcherClusterer(CSXClusterer):
            print('found similar document with id-->',matched_cluster_id)
            print('current paper id-->', current_paper.title)
            #print("----------------------")
-           #resp = Cluster.search(using=self.elastic_service.get_connection()).filter("term", _id=matched_cluster_id[0])
-           #matched_cluster = resp.execute()[0]
+           print(matched_cluster_id[0])
+           resp = Cluster.search(using=self.elastic_service.get_connection()).filter("term", _id=matched_cluster_id[0])
+           print(len(resp.execute()))
+           matched_cluster = resp.execute()[0]
            #print("hereeeee")
            #print(matched_cluster.paper_id)
-           matched_cluster = Cluster.get(using=self.elastic_service.get_connection(), id = matched_cluster_id)
+           #matched_cluster = Cluster.get(using=self.elastic_service.get_connection(), id = matched_cluster_id)
         except Exception as ex:
             print('error here in  Cluster get --->', ex)
         if current_paper.has_pdf and matched_cluster.is_citation:
