7f1129024dd476a63d4901f3db864a21d5f2b6ca
Manoj Kumar Angadi <manojkumar11930@gmail.com>
2023-01-31 18:57:58 -0500

    extracting citations from files

--- src/extraction/core.py
+++ src/extraction/core.py
@@ -265,6 +265,7 @@ def _real_run(runnables, runnable_props, data, output_dir, **kwargs):
          result = results[runnable]
          if isinstance(result, RunnableError): any_errors = True
          _output_result(runnable, result, output_dir, run_name, file_prefix=file_prefix, write_dep_errors=write_dep_errors)
+   print("results--------->")
    result_logger.info('{0} finished {1}'.format(run_name, '[SUCCESS]' if not any_errors else '[WITH ERRORS]'))
    logger_core.info('{0} finished {1}'.format(run_name, '[SUCCESS]' if not any_errors else '[WITH ERRORS]'))
 
