4f37caa111c365322f36822a28f6c611d2696408
Manoj Kumar Angadi <manojkumar11930@gmail.com>
2023-04-12 21:08:20 -0400

    combining extraction, clustering and ingestion into a single branch

--- src/models/elastic_models.py
+++ src/models/elastic_models.py
@@ -44,6 +44,80 @@ class KeyMap(Document):
     class Index:
         name = settings.KEYMAP_INDEX
 
+class Cluster_new(Document):
+    paper_id = Keyword(multi=True)
+    csx_doi = Keyword()
+    title = Text()
+    cluster_id = Keyword()
+    title_suggest = Completion()
+    text = Text()
+    has_pdf = Boolean()
+    abstract = Text()
+    is_citation = Boolean()
+    created_at = Date(default_timezone='UTC')
+    authors = Nested(type='authors')
+    self_cites = Integer()
+    num_cites = Integer()
+    cited_by = Keyword(multi=True)
+    keys = Keyword(multi=True)
+    keywords = Keyword(multi=True)
+    pub_info = Nested(type='pub_info')
+    source_url = Keyword(multi=True)
+
+    class Index:
+        name = settings.CLUSTERS_INDEX
+
+    def add_cited_by(self, paper_id: str):
+        if not self.__contains__("cited_by"):
+            self.__setitem__("cited_by", [paper_id])
+            return
+        self.cited_by.append(paper_id)
+
+    def get_cites(self):
+        if not self.__contains__("cited_by"):
+            return []
+        else:
+            return self.cited_by
+
+    def get_paper_ids(self):
+        if not self.__contains__("paper_id"):
+            return []
+        else:
+            return self.paper_id
+
+    def add_key(self, key: str):
+        if not self.__contains__("keys"):
+            self.__setitem__("keys", [key])
+            return
+        self.keys.append(key)
+
+    def add_paper_id(self, paper_id: str):
+        if not self.__contains__("paper_id"):
+            self.__setitem__("paper_id", [paper_id])
+            return
+        self.paper_id.append(paper_id)
+
+
+    def add_source_url(self, source_url: str):
+        if not self.__contains__("source_url"):
+            self.__setitem__("source_url", [source_url])
+            return
+        self.source_url.append(source_url)
+
+    def extend_keys(self, keys: List[str]):
+        if not self.__contains__("keys"):
+            self.__setitem__("keys", keys)
+            return
+        self.keys.extend(keys)
+
+    def save(self, **kwargs):
+        if self.title is not None:
+            self.title_suggest = {
+                'input': [self.title],
+            }
+        self.text_status = "fresh"
+        return super().save(**kwargs)
+
 class Cluster(Document):
     paper_id = Keyword(multi=True)
     csx_doi = Keyword()
