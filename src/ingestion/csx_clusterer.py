7d973ceaba9e6e81cd474b3a1664fd88573e19e8
Manoj Kumar Angadi <manojkumar11930@gmail.com>
2023-04-12 20:38:21 -0400

    combining extraction, clustering and ingestion into a single branch

--- src/ingestion/csx_clusterer.py
+++ src/ingestion/csx_clusterer.py
@@ -189,6 +189,7 @@ class KeyMatcherClusterer(CSXClusterer):
            except Exception:
             pass
            resp = Cluster.search(using=self.elastic_service.get_connection()).filter("term", _id=matched_cluster_id[0])
+           print(matched_cluster_id[0])
            print("jheeree in response get")
            print(resp)
            matched_cluster = resp.execute()[0]
